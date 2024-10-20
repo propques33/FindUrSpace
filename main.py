1512b6f23eb3c3bd3aa43fca22de68196a8c76fa
nishantdeswal1810 <msa23010@iiitl.ac.in>
2024-10-20 22:37:42 +0530

    done

--- main.py
+++ main.py
@@ -41,4 +41,4 @@ if __name__ == '__main__':
     
     # Use the PORT environment variable if available, default to 5000
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
