d5f18170d6c96c085152fbc0e2aaa977367bee82
propques33 <buzz@propques.com>
2024-10-22 23:03:27 +0530

    Update database.py

--- core/database.py
+++ core/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 # MongoDB connection setup
 def get_db():
-    mongo_uri = os.environ.get('MONGO_URI')  # Ensure the MONGO_URI is defined in your .env file
+    mongo_uri = os.environ.get('MONGO_URI_1')  # Ensure the MONGO_URI is defined in your .env file
     client = MongoClient(mongo_uri)
     db = client['FindYourSpace']
     return db
